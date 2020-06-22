2dc6c88cfe8c982ce73ff55332da4c21d8e88283
JD <javitolin@Gmail.com>
2020-06-22 20:31:25 +0300

    Add if raspberry to configuration getter.

--- RPIWaterSystem/ConfigurationHelper.py
+++ RPIWaterSystem/ConfigurationHelper.py
@@ -1,15 +1,17 @@
 from Consts import LocalConfigurationFile, RemoteConfigurationFile
-from IOHelper import ReadFromFile
+from IOHelper import *
 from Configuration import *
 import urllib.request
 import json
 
 def GetConfiguration():
-    #f = urllib.request.urlopen(RemoteConfigurationFile)
-    #mybytes = f.read()
-    #configurationFileContent = mybytes.decode("utf8")
     try:
-        configurationFileContent = ReadFromFile(LocalConfigurationFile)
+        if IOHelper.IsInRaspberry():
+            f = urllib.request.urlopen(RemoteConfigurationFile)
+            mybytes = f.read()
+            configurationFileContent = mybytes.decode("utf8")
+        else:
+            configurationFileContent = ReadFromFile(LocalConfigurationFile)
         configuration = Configuration.from_json(json.loads(configurationFileContent))
         return configuration
     except:
